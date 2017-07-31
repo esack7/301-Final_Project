3019b04d8cc53c25ba29bf44b21d482ff283d73c
esack7 <isaacheist@gmail.com>
2017-07-31 16:36:24 -0700

    refixed script routes again

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
-const pg = require('pg');
-const fs = require('fs');
+// const pg = require('pg');
+// const fs = require('fs');
 const express = require('express');
-const bodyParser = require('body-parser');
-const requestProxy = require('express-request-proxy');
+// const bodyParser = require('body-parser');
+// const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.static('./public'));
@@ -13,12 +13,12 @@ app.listen(PORT, function() {
   console.log(`Listening on port: "${PORT}"`);
 });
 
-
-function proxytheMoviedb(request, response) {
-  console.log('Routing GitHub request for', request.params);
-  (requestProxy({
-    url: 'https://api.themoviedb.org/3/discover/movie?api_key=' + `${process.env.theMoviedb_Token}`,
-  }))(request, response);
-}
-
-proxytheMoviedb();
+//
+// function proxytheMoviedb(request, response) {
+//   console.log('Routing GitHub request for', request.params);
+//   (requestProxy({
+//     url: 'https://api.themoviedb.org/3/discover/movie?api_key=' + `${process.env.theMoviedb_Token}`,
+//   }))(request, response);
+// }
+//
+// proxytheMoviedb();
