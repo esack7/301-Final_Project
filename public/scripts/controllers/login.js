0ece8da9dc926bf37ea068b472ebf5fc59f97357
kimmergithub <kimmer@atomicprofits.com>
2017-07-31 13:52:00 -0700

    fixing everyones problems

--- public/scripts/controllers/login.js
+++ public/scripts/controllers/login.js
@@ -1,14 +1,3 @@
 'use strict';
 
 var app = app || {};
-
-$('#about').hide();
-$('#displayMovie').hide();
-$('#main').show();
-
-$('#initiate').submit(function (event) {
-  event.preventDefault();
-  $('#initiate').hide();
-  $('#initiate').hide();
-  $('#main').show();
-});
