ac7d36a5e927d953d2f0e30125c63046b739a936
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-08-01 09:59:55 -0700

    Got server working sorta

--- server.js
+++ server.js
@@ -2,21 +2,34 @@
 const pg = require('pg');
 // const fs = require('fs');
 const express = require('express');
+// const request = require('request');
+// const querystring = require('querystring');
 const app = express();
 const bodyParser = require('body-parser');
-const requestProxy = require('express-request-proxy').urlencoded({extended: true});
+const requestProxy = require('express-request-proxy')
+// .urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('.'))
+// app.use(express.static('.'))
+
+
+app.use(express.static('./public'))
+    .get('*',function (req,res) {
+      res.sendFile('index.html');
+    })
+ // .listen(3000);
 
 //This will be our future login information sending to PS
 app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
+// 
+// app.get('/', function(request, response) {response.sendFile('index.html', {root:'./'});
+// });
 
-app.get('/', function(request, response) {response.sendFile('index.html', {root:'./'});
-});
 
+// const client_id = process.env.CLIENT_ID;
+// const redirect_uri = process.env.REDIRECT_URI;
 
 
 function proxytheMoviedb(request, response) {
