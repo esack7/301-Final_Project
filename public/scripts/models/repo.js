d23a7abf8db6e791bb3d99feb29302fd59b7553e
kimmergithub <kimmer@atomicprofits.com>
2017-08-01 18:19:54 -0700

    got data... now loading data

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -1,25 +1,46 @@
 'use strict';
 var app = app || {};
 
-
-let vote_average = 8
-let genre = 20
-let runtime = 90;
-
-
-
-var query  = 'https://api.themoviedb.org/3/discover/movie?api_key=8f303e30bafa7db2b3656c65726f874c&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&' + 'vote_average.lte=' + vote_average + '&with_genres=' + genre + '&with_runtime.gte=' + runtime;
-
-
 (function(module) {
   const repos = {};
+// vote average needs to take in a number with a decimal
+  let vote_average;
+  // genre takes in movie genre...
+  let genre;
+  let genreInput;
+
+  // runtime
+  let runtime;
+  let query;
+
+  let genreObject = {'28': 'Action', '12': 'Adventure', '16': 'Animation', '35': 'Comedy', '80': 'Crime', '99': 'Documentary', '18': 'Drama', '10751': 'Family', '14': 'Fantasy', '36': 'History', '27': 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War', 37: 'Western'};
+
+
+  $('#topThree').click(function (event) {
+    console.log('topThree BUTTON FIRED!!!');
+    repos.all = [];
+    console.log('app.repos.all is CLEARED!');
+    event.preventDefault();
+    vote_average = $('#voteAverageDropDown').val();
+    genreInput = $('#genreDropDown').val();
+    console.log(genreObject);
+    console.log('genreInput = ' + genreInput);
+    genre = Object.values(genreObject)[Object.keys(genreObject).indexOf(genreInput)];
+    runtime = $('#durationDropDown').val();
+    console.log('vote_average = ' + vote_average + '  genre = ' + genre + '  runtime = ' + runtime);
+    query = 'https://api.themoviedb.org/3/discover/movie?api_key=8f303e30bafa7db2b3656c65726f874c&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&' + 'vote_average.lte=' + vote_average + '&with_genres=' + genreInput + '&with_runtime.gte=' + runtime;
+    repos.requestMovie();
+    console.log(' ');
+    console.log(repos.all);
+  });
+
+  // function randomTopThree() {
+  //   console.log(Object.values(Object.values(app.repos.all[0])[3][0])[4])
+  //   let getTitle = Object.values(Object.values(app.repos.all[0])[3][0])[4]
+  // }
+
   console.log('isaac is here!');
   repos.all = [];
-  // let repos.rawData;
-  // let page = 1;
-  // let vote_average = 7
-  // let genre = 18
-  // let runtime = 60;
   repos.requestMovie = function(callback) {
     var settings = {
       'async': true,
@@ -29,20 +50,9 @@ var query  = 'https://api.themoviedb.org/3/discover/movie?api_key=8f303e30bafa7d
     }
 
     $.get(settings).then(function (response) {
-      repos.all = response;
+      repos.all.push(response);
     })
     .then(callback);
-    // $.get('&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_average.gte=7&with_genres=18&with_runtime.gte=60')
-    // .then(
-    //
-    //       function(data) {
-    //         repos.all = data;
-    //       },
-    //       function(error) {
-    //         console.log('error', error);
-    //       }
-    //     )
-    //     .then(callback)
   };
 
   module.repos = repos;
