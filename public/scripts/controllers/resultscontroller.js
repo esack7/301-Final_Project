6c5e9b638b1c04456a467358f585374fe51047d7
Shaun Wahle <YOUR EMAIL ADDRESS>
2017-08-04 07:20:31 -0700

    Fixing project domain

---
+++ public/scripts/controllers/resultscontroller.js
@@ -0,0 +1,13 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const resultsController = {};
+  resultsController.init = function (){
+    $('#tab-content').hide();
+    $('#about').show();
+
+  }
+  module.resultsController = resultsController;
+
+})(app);
